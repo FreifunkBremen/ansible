7f089d5a32c34397f7387f2d966d77cced13a3a3
Moritz Kaspar Rudert (mortzu) <me@mortzu.de>
2015-11-23 12:13:28 +0100

    Renamed routing table

--- roles/exit/templates/openvpn/exit-up.sh
+++ roles/exit/templates/openvpn/exit-up.sh
@@ -4,4 +4,4 @@
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
 ip route add $trusted_ip via $route_net_gateway
-ip route add default via $route_vpn_gateway dev $1 table {{ batman_table }}
+ip route add default via $route_vpn_gateway dev $1 table {{ ffhb_routing_table }}
