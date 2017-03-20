0fbce51e8ee6e2317b9a0e851d32ef1b424e8466
Geno <genofire@users.noreply.github.com>
2017-03-20 18:37:54 +0100

    Allow nodes to send respondd packages
    This will show the vpn name in node-status-page, again.

--- roles/respondd/templates/firewall.sh
+++ roles/respondd/templates/firewall.sh
@@ -1,1 +1,3 @@
 ipt6 -A INPUT -i {{ main_bridge }} -p udp --dport 1001 -j ACCEPT
+ipt6 -A INPUT -i vpn-{{ site_code }}-legacy -p udp --dport 1001 -j ACCEPT
+ipt6 -A INPUT -i vpn-{{ site_code }} -p udp --dport 1001 -j ACCEPT
