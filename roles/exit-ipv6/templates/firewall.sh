a7c4b6354054fa454d32c718a77765ace2047097
Jan-Philipp Litza <janphilipp@litza.de>
2018-02-24 14:30:55 +0100

    exit-ipv6: Allow traffic from the mesh into the mesh
    This allows, for example, for BGP peerings via the mesh

--- roles/exit-ipv6/templates/firewall.sh
+++ roles/exit-ipv6/templates/firewall.sh
@@ -1,2 +1,3 @@
 ipt6 -A FORWARD -i {{ main_bridge }} -o {{ exit_ipv6_interface }} -j ACCEPT
 ipt6 -A FORWARD -o {{ main_bridge }} -i {{ exit_ipv6_interface }} -j ACCEPT
+ipt6 -A FORWARD -i {{ main_bridge }} -o {{ main_bridge }} -j ACCEPT
