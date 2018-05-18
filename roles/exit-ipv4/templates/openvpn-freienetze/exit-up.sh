46bff400128a4ab154614ac0ab782341a53596c7
Jan-Philipp Litza <janphilipp@litza.de>
2018-05-19 01:17:43 +0200

    exit-ipv4: Remove static address of Freie Netze e.V.
    It seems to change, so only use device

--- roles/exit-ipv4/templates/openvpn-freienetze/exit-up.sh
+++ roles/exit-ipv4/templates/openvpn-freienetze/exit-up.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 # {{ ansible_managed }}
 
-exec /sbin/ip route add default via 172.31.240.1 dev "$1" table {{ ffhb_routing_table }}
+exec /sbin/ip route add default dev "$1" table {{ ffhb_routing_table }}
