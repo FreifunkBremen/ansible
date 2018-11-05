170bbcb559abbec6d76fb1985875c50be037c070
Oliver Gerlich <oliver.gerlich@gmx.de>
2018-11-05 03:24:48 +0100

    backup webserver with Restic (#85)
    * add restic role
    
    This role downloads Restic 0.8.3, installs it to /opt/restic/, and creates
    a symlink in /usr/local/bin/.
    
    * add backup-webserver role
    
    This role installs a backup script (tailored for the webserver) which uses
    Restic to back up nearly the entire host.
    
    * reduce Restic log output
    
    * mark cron script as executable
    
    * extend README
    
    * run "influxd backup" first, to export InfluxDB data to backup-friendly format
    
    * move Restic repository config from cronjob into /etc/backup.d/
    
    * use flock to ensure that only one backup runs at a time
    
    * exclude gatemon data files from backup
    
    These files are recreated often, and this fixes some "file has changed"
    warnings from Restic.
    
    * state desired Restic version in separate defaults/main.yml file
    
    * upgrade to Restic 0.9.1

---
+++ roles/backup-webserver/files/restic_backup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#
+# Creates backup of entire webserver using Restic.
+#
+# Selected data is excluded from backup.
+#
+# Parameters:
+# 1: target repository (ie. the "-r" parameter for restic)
+# 2: path to password file (full path to file containing the repository password)
+#
+
+repository=$1
+passwordFile=$2
+
+restic \
+    --repo "$repository" \
+    --password-file "$passwordFile" \
+    --one-file-system \
+    --quiet \
+    --exclude=/var/www/downloads/domains/downloads.bremen.freifunk.net/ \
+    --exclude=/var/www/gatemon/domains/status.bremen.freifunk.net/data/ \
+    --exclude=/var/log/journal/ \
+    --exclude="/home/tiles/*.mbtiles" \
+    --exclude="/home/tiles/*.mbtiles3" \
+    --exclude=/var/lib/grafana/png/ \
+    --exclude=/var/lib/influxdb/ \
+    --exclude=/var/cache/ \
+    --exclude=/root/.cache/ \
+    --exclude="/home/*/.cache" \
+    --exclude=/tmp/ \
+    --exclude=/var/tmp/ \
+    backup /
