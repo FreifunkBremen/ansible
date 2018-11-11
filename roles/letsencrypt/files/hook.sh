6a5b15985bb6ac0ea9ad84b417eb2d284bdb85c0
Oliver Gerlich <oliver.gerlich@gmx.de>
2018-11-11 17:10:54 +0100

    letsencrypt: add changes made on webserver

--- roles/letsencrypt/files/hook.sh
+++ roles/letsencrypt/files/hook.sh
@@ -1,24 +1,8 @@
 #!/usr/bin/env bash
 
 case "$1" in
-    deploy_challenge)
-        ;;
-    clean_challenge)
-        ;;
     deploy_cert)
         systemctl reload apache2
-        if [ -d /etc/.git ]; then
-            cd "$(dirname "$3")"
-            git add .
-            git commit -m "letsencrypt: refreshed certificate for $2" .
-        fi
-        ;;
-    unchanged_cert)
-        ;;
-    *)
-        echo "Unknown hook $1"
-        exit 1
-        ;;
 esac
 
 exit 0
