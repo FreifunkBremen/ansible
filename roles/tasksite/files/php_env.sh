e0560109435528002b7999813c24a0b9a3a49004
Oliver Gerlich <oliver.gerlich@gmx.de>
2018-11-11 22:24:51 +0100

    tasksite: fix this role so it can be used on the current webserver

--- roles/tasksite/files/php_env.sh
+++ roles/tasksite/files/php_env.sh
@@ -1,2 +1,2 @@
 # sets environment variables necessary to correctly execute Phabricator PHP scripts
-export PHP_INI_SCAN_DIR="/etc/php5/cgi/conf.d:${HOME}/.config/etc/php"
+export PHP_INI_SCAN_DIR=":${HOME}/.config/etc/php"
