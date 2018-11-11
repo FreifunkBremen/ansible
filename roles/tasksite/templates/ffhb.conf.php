6017c07e5dd35e80d22b508938d5e489671dd3f1
Oliver Gerlich <oliver.gerlich@gmx.de>
2018-11-11 22:17:33 +0100

    tasksite: redirect tasks.bremen.freifunk.net to tasks.ffhb.de
    Phabricator only supports a single base domain, and we have used
    tasks.ffhb.de for this so far. So make tasks.bremen.freifunk.net a redirect
    rather than serving the same content under both domains.

--- roles/tasksite/templates/ffhb.conf.php
+++ roles/tasksite/templates/ffhb.conf.php
@@ -7,7 +7,8 @@ return array(
   'mysql.user' => 'phabric',
   'mysql.pass' => '{{ mysql_password_base64["content"] | b64decode }}',
   'phabricator.timezone' => 'Europe/Berlin',
-  'phabricator.base-uri' => 'https://{{ tasks_domain }}/',
+  # Phabricator only supports one single base URI, and we have used the shorter one (tasks.ffhb.de) so far:
+  'phabricator.base-uri' => 'https://{{ tasks_subdomain }}.{{ alt_domain }}/',
   'repository.default-local-path' => '/home/{{ tasks_user }}/.var/repo',
   'storage.local-disk.path' => '/home/{{ tasks_user }}/.var/storage',
 );
