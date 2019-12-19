821483d51d34bf663ed99f712dafceca9bbc4b19
Jan-Philipp Litza <janphilipp@litza.de>
2019-12-19 11:31:38 +0100

    fixup! Comply with shellcheck everywhere

--- roles/exit-ipv4/templates/default/up.sh
+++ roles/exit-ipv4/templates/default/up.sh
@@ -2,7 +2,7 @@
 
 # Copy default route
 if [ "$IFACE" = "{{ ansible_default_ipv4.interface }}" ]; then
-  if ! ip -4 route list 0/0 table '{{ ffhb_routing_table }}' | grep q '^default'; then
+  if ! ip -4 route list 0/0 table '{{ ffhb_routing_table }}' | grep -q '^default'; then
     ip -4 route list 0/0 | xargs ip route add table '{{ ffhb_routing_table }}'
   fi
 fi
