e294b9bb8d3630dcb968c78e44bb24b2b3ef6765
mortzu <mortzu@uni-bremen.de>
2021-08-03 08:51:42 +0200

    Removed OpenVPN tunnel configuration for IPv4

--- roles/exit-ipv4/templates/openvpn-freienetze/exit-up.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env sh
-# {{ ansible_managed }}
-
-exec /sbin/ip route add default dev "$1" table '{{ ffhb_routing_table }}'
--- roles/exit-ipv4/templates/openvpn-hideme/exit-up.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-# {{ ansible_managed }}
-
-# shellcheck disable=SC2154
-{
-    /sbin/ip route add "$trusted_ip" via "$route_net_gateway"
-    /sbin/ip route add default via "$route_vpn_gateway" dev "$1" table '{{ ffhb_routing_table }}'
-}
