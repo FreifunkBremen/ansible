0038d2dec12997ee5b3594ecb0ed019e998528ff
Jan-Philipp Litza <janphilipp@litza.de>
2023-10-15 20:16:43 +0200

    downlink-firewall: Exclude Deleted ip mon lines
    Also, optimize by using case statement

--- roles/downlink-firewall/files/ipv6-neigh-cache-negative.sh
+++ roles/downlink-firewall/files/ipv6-neigh-cache-negative.sh
@@ -14,7 +14,12 @@ IPSET_HASHSIZE=4096
 ipset -exist create "$IPSET_NAME" hash:net family inet6 hashsize "$IPSET_HASHSIZE" timeout "$IPSET_TIMEOUT"
 
 ip -6 monitor neigh | while read -r LINE; do
-    [ "${LINE##* }" = "FAILED" ] || continue
-    [ "${LINE#fe80::}" = "$LINE" ] || continue
-    printf "add -exist %s %s\nCOMMIT\n" "$IPSET_NAME" "${LINE%% *}"
+    case "$LINE" in
+        fe80::*|"Deleted "*)
+            continue
+            ;;
+        *" FAILED")
+            printf "add -exist %s %s\nCOMMIT\n" "$IPSET_NAME" "${LINE%% *}"
+            ;;
+    esac
 done | ipset restore
