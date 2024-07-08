77b15e1bd75c5c0b62369b0a2d2674b50435ebc2
Jan-Philipp Litza <janphilipp@litza.de>
2024-07-08 18:06:52 +0200

    lib/hosts.py: Use builtin ipaddress module instead of ipcalc

--- lib/hosts.py
+++ lib/hosts.py
@@ -1,137 +1,117 @@
-#
-# You need a recent version of ipcalc. On Debian/Ubuntu you can install it with:
-# sudo apt-get install python-pip
-# sudo pip install ipcalc
-#
-# Also, you need python-gnupg and python-yaml to read the secret variables
-# (which obviously also have to be encrypted for you)
-#
-from __future__ import print_function
-
-import copy
-import ipcalc
+import ipaddress
 import json
-import sys
 from slpp import slpp as lua
 
-if ipcalc.__version__.startswith("0.5."):
-  sys.stderr.write("ipcalc 0.5 ist zu alt, nutze pip es zu aktualisieren (z.B. pip install --upgrade ipcalc)")
-  sys.exit(1)
 
 class Inventory:
 
-  groups = {}
+    groups = {}
 
-  def __init__(self, site_conf, ipv6_global_network=None, ipv6_local_network=None, ipv6_uplink_network=None, icvpn_ipv4_network=None, icvpn_ipv6_network=None):
+    def __init__(
+        self,
+        site_conf,
+        ipv6_global_network=None,
+        ipv6_local_network=None,
+        ipv6_uplink_network=None,
+    ):
 
-    # read and parse site.conf
-    with open(site_conf,'r') as f:
-      self.site = lua.decode(f.read())
-      if not isinstance(self.site, dict):
-        raise TypeError("Unable to parse site.conf")
+        # read and parse site.conf
+        with open(site_conf, 'r') as f:
+            self.site = lua.decode(f.read())
+            if not isinstance(self.site, dict):
+                raise TypeError("Unable to parse site.conf")
 
-    self.ipv4_network        = ipcalc.Network(self.site["prefix4"])
-    self.icvpn_ipv4_network  = ipcalc.Network(icvpn_ipv4_network)
-    self.icvpn_ipv6_network  = ipcalc.Network(icvpn_ipv6_network)
-    self.ipv6_uplink_network = ipcalc.Network(ipv6_uplink_network)
-    self.ipv6_global_network  = ipcalc.Network(ipv6_global_network)
+        self.ipv4_network = ipaddress.IPv4Network(self.site["prefix4"])
+        self.ipv6_uplink_network = ipaddress.IPv6Network(ipv6_uplink_network)
+        self.ipv6_global_network = ipaddress.IPv6Network(ipv6_global_network)
 
-    if "prefix6" in self.site:
-      self.ipv6_local_network = ipcalc.Network(self.site["prefix6"])
-    else:
-      self.ipv6_local_network  = ipcalc.Network(ipv6_local_network)
+        if "prefix6" in self.site:
+            self.ipv6_local_network = ipaddress.IPv6Network(self.site["prefix6"])
+        else:
+            self.ipv6_local_network = ipaddress.IPv6Network(ipv6_local_network)
 
-  def group(self, name, **options):
-    group = Group(self, **options)
-    self.groups[name] = group
-    return group
+    def group(self, name, **options):
+        group = Group(self, **options)
+        self.groups[name] = group
+        return group
 
-  def data(self):
-    hostvars = {}
-    data     = {}
+    def data(self):
+        hostvars = {}
+        data = {}
 
-    for name, group in self.groups.items():
-      data[name] = {
-        "hosts":    [hostname for (hostname,_) in group.hosts],
-        "children": group.children,
-      }
-      for (hostname,vars) in group.hosts:
-        hostvars[hostname] = vars
+        for name, group in self.groups.items():
+            data[name] = {
+                "hosts":    [hostname for (hostname, _) in group.hosts],
+                "children": group.children,
+            }
+            for (hostname, vars) in group.hosts:
+                hostvars[hostname] = vars
 
-    data["_meta"] = {"hostvars": hostvars}
-    data["all"]   = {"vars": {
-      "site":                self.site,
-      "site_code":           self.site["site_code"],
-      "ipv4_network":        self.attributeString("ipv4_network"),
-      "ipv6_local_network":  self.attributeString("ipv6_local_network"),
-      "ipv6_uplink_network": self.attributeString("ipv6_uplink_network"),
-      "ipv6_global_network": self.attributeString("ipv6_global_network"),
-    }}
+        data["_meta"] = {"hostvars": hostvars}
+        data["all"] = {"vars": {
+            "site":                self.site,
+            "site_code":           self.site["site_code"],
+            "ipv4_network":        self.attributeString("ipv4_network"),
+            "ipv6_local_network":  self.attributeString("ipv6_local_network"),
+            "ipv6_uplink_network": self.attributeString("ipv6_uplink_network"),
+            "ipv6_global_network": self.attributeString("ipv6_global_network"),
+        }}
 
-    return data
+        return data
 
-  def json_dump(self, **kwargs):
-    return json.dumps(self.data(), **kwargs)
+    def json_dump(self, **kwargs):
+        return json.dumps(self.data(), **kwargs)
 
-  def attributeString(self, key):
-    value = getattr(self, key, None)
-    if value is not None:
-      value = str(value)
-    return value
+    def attributeString(self, key):
+        value = getattr(self, key, None)
+        if value is not None:
+            value = str(value)
+        return value
 
-  def calculate_address(self, key, incr):
-    try:
-      origin = getattr(self, key)
-    except AttributeError:
-      return
+    def calculate_address(self, key, incr):
+        try:
+            origin = getattr(self, key)
+        except AttributeError:
+            return
 
-    address = ipcalc.IP(origin.ip + incr)
-    return {
-      "address": str(address.to_compressed() if origin.v==6 else address),
-      "netmask": str(origin.netmask()),
-      "size":    origin.subnet(),
-    }
+        address = origin[incr]
+        return {
+            "address": str(address),
+            "netmask": str(origin.netmask),
+            "size":    origin.prefixlen,
+        }
+
 
 class Group:
-  def __init__(self, inventory, dhcp=False, icvpn=False, **vars):
-    self.inventory = inventory
-    self.dhcp      = dhcp
-    self.icvpn     = icvpn
-    self.vars      = vars
-    self.hosts     = []
-    self.children  = []
+    def __init__(self, inventory, dhcp=False, **vars):
+        self.inventory = inventory
+        self.dhcp = dhcp
+        self.vars = vars
+        self.hosts = []
+        self.children = []
 
-  def host(self, id, hostname, **host_vars):
-    vars = self.vars.copy()
-    vars.update(host_vars)
-    vars.update({
-      "vpn_id":             id,
-      "batman_ipv4":        self.calculate_address("ipv4_network", id),
-      "batman_ipv6_global": self.calculate_address("ipv6_global_network", id),
-      "batman_ipv6_local":  self.calculate_address("ipv6_local_network", id),
-    })
+    def host(self, id, hostname, **host_vars):
+        vars = self.vars.copy()
+        vars.update(host_vars)
+        vars.update({
+            "vpn_id":             id,
+            "batman_ipv4":        self.calculate_address("ipv4_network", id),
+            "batman_ipv6_global": self.calculate_address("ipv6_global_network", id),
+            "batman_ipv6_local":  self.calculate_address("ipv6_local_network", id),
+        })
 
-    if self.dhcp:
-      begin = self.inventory.ipv4_network.ip + (id << 8)*10
-      vars["dhcp"] = {
-        "netmask":     str(self.inventory.ipv4_network.netmask()),
-        "range_begin": str(ipcalc.IP(begin)),
-        "range_end":   str(ipcalc.IP(begin+256*10-1)),
-      }
+        if self.dhcp:
+            begin = self.inventory.ipv4_network[(id << 8)*10]
+            vars["dhcp"] = {
+                "netmask":     str(self.inventory.ipv4_network.netmask),
+                "range_begin": str(ipaddress.IPv4Address(begin)),
+                "range_end":   str(ipaddress.IPv4Address(begin + 256 * 10 - 1)),
+            }
 
-    if self.icvpn:
-      vars["icvpn_ipv4"] = self.calculate_address("icvpn_ipv4_network", (id << 8))
-      vars["icvpn_ipv6"] = self.calculate_address("icvpn_ipv6_network", (id << 16))
-      vars["icvpn_ipv4_network"] = self.inventory.attributeString("icvpn_ipv4_network")
-      vars["icvpn_ipv6_network"] = self.inventory.attributeString("icvpn_ipv6_network")
+        self.hosts.append((hostname, vars))
 
-    vars["ipv6_uplink_own_gateway"] = self.calculate_address("ipv6_uplink_network", (id << 16*4)+1)
-    vars["ipv6_uplink_own_vpnserver"] = self.calculate_address("ipv6_uplink_network", (id << 16*4)+2)
+    def calculate_address(self, *args):
+        return self.inventory.calculate_address(*args)
 
-    self.hosts.append((hostname, vars))
-
-  def calculate_address(self, *args):
-    return self.inventory.calculate_address(*args)
-
-  def child(self, child):
-    self.children.append(child)
+    def child(self, child):
+        self.children.append(child)
